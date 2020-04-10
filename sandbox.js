b5f7381522770873e941a2ea0f6a8ecc038802ac
sujan <sujanpoojary884@gmail.com>
2020-04-10 15:20:21 +0530

    methods and chaining property

---
+++ sandbox.js
@@ -0,0 +1,51 @@
+// object literal notation - baisc
+/*
+const userOne = {
+  username: 'sujan',
+  email: 'sujan@gmail.com',
+  login() {
+    console.log('user loggedin');
+  },
+  logout() {
+    console.log('the user logged out');
+  },
+};
+
+console.log(userOne.email);
+
+userOne.login();
+userOne.logout();
+*/
+
+/* Classes and object */
+
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+    this.score = 0;
+  }
+  login() {
+    console.log(`${this.username} is just loggedin`);
+    return this;
+  }
+  logout() {
+    console.log(`${this.username} is just logged out`);
+    return this;
+  }
+  incScore() {
+    this.score += 1;
+    console.log(`${this.username} has a score ${this.score}`);
+  }
+}
+
+// craete new user and method chaining
+const userOne = new User('max', 'maximilina@gmail.com');
+userOne.login().logout();
+userOne.incScore();
+console.log(userOne);
+
+// new keyword
+// 1 - it cretaes a new empty object {}
+// 2 - it binds the value of 'this' to the new empty object
+// 3 - it calls the constructor fucntion to build a new object
