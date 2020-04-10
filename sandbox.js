cd7cc51a430611a7addcc9209e293d340bbeccce
sujan <sujanpoojary884@gmail.com>
2020-04-10 15:05:54 +0530

    class and object brief intro done.!

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,8 @@
-// object literal notation
+// object literal notation - baisc
+/*
 const userOne = {
   username: 'sujan',
-  email: 'sujan@gmaol.com',
+  email: 'sujan@gmail.com',
   login() {
     console.log('user loggedin');
   },
@@ -14,3 +15,21 @@ console.log(userOne.email);
 
 userOne.login();
 userOne.logout();
+*/
+
+/* Classes and object */
+
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
+}
+
+const userOne = new User('max', 'maximilina@gmail.com');
+console.log(userOne);
+
+// new keyword
+// 1 - it cretaes a new empty object {}
+// 2 - it binds the value of 'this' to the new empty object
+// 3 - it calls the constructor fucntion to build a new object
